4e2693261904d9485054241c18b986a8ba403ce4
ketan singh <ketansingh1@districtd.in>
2024-05-16 08:07:38 +0530

    feat: assignments solutions added

---
+++ week 6/understandingHooks/src/App.jsx
@@ -0,0 +1,18 @@
+  function App() {
+  return (
+    <>
+      <Header title="ketan 1" />
+      <Header title="ketan 2" />
+    </>
+  )
+}
+
+function Header({title}) {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  )
+}
+
+export default App
